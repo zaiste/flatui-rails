00c134fd35d11406762af6bb288c9fb053f42647
Piotrek <pkurek90@gmail.com>
2013-03-12 21:23:56 +0100

    added fonts, images and javascripts

---
+++ vendor/assets/javascripts/flat-ui/flat-ui.js
@@ -0,0 +1,10 @@
+//= require flat-ui/components/bootstrap-tooltip
+//= require flat-ui/components/custom_radio
+//= require flat-ui/components/custom_checkbox_and_radio
+//= require flat-ui/components/html5shiv
+//= require flat-ui/components/icon-font-ie7
+//= require flat-ui/components/jquery.dropkick-1.0.0.js
+//= require flat-ui/components/jquery.placeholder.js
+//= require flat-ui/components/jquery.tagsinput
+//= require flat-ui/components/lte-ie7-24
+//= require flat-ui/components/application
