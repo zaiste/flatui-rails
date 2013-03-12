76a9607b79b132c1968fd8676d7c5395915b95aa
Piotrek <pkurek90@gmail.com>
2013-03-12 22:24:16 +0100

    removed js components, they werent really working well

--- vendor/assets/javascripts/flat-ui/flat-ui.js
+++
@@ -1,8 +0,0 @@
-//= require flat-ui/components/bootstrap-tooltip
-//= require flat-ui/components/custom_radio
-//= require flat-ui/components/custom_checkbox_and_radio
-//= require flat-ui/components/html5shiv
-//= require flat-ui/components/icon-font-ie7
-//= require flat-ui/components/jquery.placeholder.js
-//= require flat-ui/components/jquery.tagsinput
-//= require flat-ui/components/application
